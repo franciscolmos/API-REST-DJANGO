7eb27b5ffe00cfe1b0cf49e65a7ba055a0b58d69
franciscolmos <francisco.olmos.ubp@gmail.com>
2021-08-22 20:20:19 -0300

    [ADDED] - NOW YOU CAN FILTER BY NAME OR EMAIL

--- ecommerce/profiles_api/views.py
+++ ecommerce/profiles_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from profiles_api import serializers
@@ -11,3 +11,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
