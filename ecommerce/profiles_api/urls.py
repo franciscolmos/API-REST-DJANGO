65dfb13ed5b9d14381eb8f0ebc3dbf06f30d3d0d
franciscolmos <francisco.olmos.ubp@gmail.com>
2021-08-21 23:30:17 -0300

    [ADDED] - FIRST VIEWS.PY AND ENDPOINT

--- ecommerce/profiles_api/urls.py
+++ ecommerce/profiles_api/urls.py
@@ -1,11 +1,8 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from profiles_api import views
-#
-# router = DefaultRouter()
-# router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-#
-# urlpatterns = [
-#     path('hello-view/', views.HelloApiView.as_view()),
-#     path('', include(router.urls))
-# ]
+from django.urls import path
+
+from profiles_api import views
+
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+]
--- ecommerce/profiles_api/views.py
+++ ecommerce/profiles_api/views.py
@@ -1,30 +1,18 @@
-# from django.shortcuts import render
-# from rest_framework.views import APIVIew
-# from rest_framework import status
-# from rest_framework import viewsets
-# # from rest_framework.authentications import TokenAuthentication
-# #
-# # from profiles_api import serializers
-# # from profiles_api import models
-# # from profiles_api import permissions
-# #
-# #
-# # class HelloApiView(ApiView):
-# #     """Test API View"""
-# #     serializers_class = serializers.HelloSerializer
-# #
-# #     def get(self, request, format=None):
-# #         """Returns a list of APIView feature"""
-# #         an_apiview = [
-# #             'huevada 1',
-# #             'huevada 2',
-# #             'huevada 3',
-# #             'huevada 4',
-# #         ]
-# #
-# #     class UserProfileViewSet(viewsets.ModelViewSet):
-# #         """CONTROLADOR DE CREADO Y ACTUALIZADO DE PERFILES"""
-# #         serializers_class = serializers.UserProfileSerializer
-# #         queryset = models.UserProfile.objects.all()
-# #         authentication_classes = (TokenAuthentication, )
-# #         permissions_classes = (permissions.UpdateOwnProfile,)
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
